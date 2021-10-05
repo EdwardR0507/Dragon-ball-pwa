9d90b4699375e4a313011ac3823dbb94be4ca997
EdwardR0507 <ramosedward21@gmail.com>
2021-10-04 22:35:08 -0500

    fix: redirect

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router";
+import { useParams, Redirect } from "react-router";
 import { Characters } from "../models/Characters";
 const Character = ({ history }) => {
   const { id } = useParams();
@@ -7,23 +7,30 @@ const Character = ({ history }) => {
     history.goBack();
   };
 
-  const { species, name, description } = Characters.find(
-    (character) => character.id === id
-  );
+  const characterSelected = Characters.find((character) => character.id === id);
+  if (!characterSelected) {
+    return <Redirect to="/" />;
+  }
+  const { species, name, description } = characterSelected;
   const path = `/assets/${species[0].toLowerCase()}-${id}.png`;
   return (
-    <div className="container mx-auto my-5 row">
-      <div className="col-12 col-md-6">
-        <img className="img-fluid d-block mx-auto" src={path} alt={id} />
+    { characterSelected } && (
+      <div className="container mx-auto my-5 row">
+        <div className="col-12 col-md-6">
+          <img className="img-fluid d-block mx-auto" src={path} alt={id} />
+        </div>
+        <div className="col-12 col-md-5 d-block my-auto">
+          <h1 className="mt-3 text-center">{name}</h1>
+          <p>{description}</p>
+          <button
+            className="btn btn-outline-warning col-12"
+            onClick={handleBack}
+          >
+            Go Back
+          </button>
+        </div>
       </div>
-      <div className="col-12 col-md-5 d-block my-auto">
-        <h1 className="mt-3 text-center">{name}</h1>
-        <p>{description}</p>
-        <button className="btn btn-outline-warning col-12" onClick={handleBack}>
-          Go Back
-        </button>
-      </div>
-    </div>
+    )
   );
 };
 export default Character;
--- src/routes/HomeRouter.jsx
+++ src/routes/HomeRouter.jsx
@@ -5,10 +5,12 @@ import Home from "../pages/Home";
 import { AuthContext } from "../context/AuthContext";
 import PublicRouter from "./PublicRouter";
 import PrivateRouter from "./PrivateRouter";
+import ScrollToTop from "../components/ScrollToTop";
 const HomeRouter = () => {
   const { isAuthenticated } = useContext(AuthContext);
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <PublicRouter path="/home" auth={isAuthenticated} component={Home} />
         <PrivateRouter path="/" auth={isAuthenticated} component={AppRouter} />
