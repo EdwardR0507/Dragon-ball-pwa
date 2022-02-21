f4041154682f303e490c1e98d8ea44357ddf4766
EdwardR0507 <ramosedward21@gmail.com>
2022-02-21 00:26:34 -0500

    test:Test on helpers(100% coverage)

--- src/helpers/getCharacterByName.test.js
+++ src/helpers/getCharacterByName.test.js
@@ -4,8 +4,8 @@ describe("Tests on getCharacterByName", () => {
     const character = getCharacterByName("Goku");
     expect(character).toHaveLength(1);
   });
-  test("Should be return a empty array", () => {
-    const character = getCharacterByName("Pikachu");
+  test("Should be return a empty array if name is empty", () => {
+    const character = getCharacterByName("");
     expect(character).toHaveLength(0);
   });
 });
