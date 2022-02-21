b64ba2df947bd51732c740adc2b6610883e700a9
EdwardR0507 <ramosedward21@gmail.com>
2022-02-21 00:11:42 -0500

    test: Test on AllCharacters page (100% coverage)

--- src/pages/AllCharacters.test.js
+++ src/pages/AllCharacters.test.js
@@ -1,15 +1,13 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { render, screen, prettyDOM } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import AllCharacters from "./AllCharacters";
 
 describe("Test on <AllCharacters />", () => {
   test("should be render", () => {
     render(
-      <MemoryRouter initialEntry={`/`}>
-        <Routes>
-          <Route path="/" element={<AllCharacters />} />
-        </Routes>
+      <MemoryRouter initialEntry={["/characters"]}>
+        <AllCharacters />
       </MemoryRouter>
     );
     expect(screen.getByText("All Characters")).toBeInTheDocument();
